89e218912c4d8329449322d8da859247cacdb696
Vivek Malneedi <vivekmalneedi@gmail.com>
2018-02-24 19:08:16 -0600

    raw tcp access to read protobufs

---
+++ client.py
@@ -0,0 +1,63 @@
+import socket
+import os
+from subscribe_pb2 import Subscribe
+from messages import *
+from enum import Enum
+from constants import _SUBSCRIBE, MAGIC_HEADER, SIZE_HEADER
+
+
+class MsgType(Enum):
+    LIGHT_STATE = 0
+    PACMAN_LOCATION = 1
+    FULL_STATE = 2
+
+message_buffers = {
+    MsgType.FULL_STATE: PacmanState,
+    MsgType.PACMAN_LOCATION: PacmanState.AgentState,
+    MsgType.LIGHT_STATE: LightState
+}
+
+
+def pack_msg(msg, msg_type):
+    if msg_type == _SUBSCRIBE:
+        header = SIZE_HEADER.pack(MAGIC_HEADER, msg_type, len(msg))
+    else:
+        header = SIZE_HEADER.pack(MAGIC_HEADER, msg_type.value, len(msg))
+    return header + msg
+
+
+def subscribe(msg_types, direction):
+        msg = Subscribe()
+        '''
+        for msg_type in msg_types:
+            msg.msg_types.append(msg_type.value)
+        '''
+        msg.msg_types.append(MsgType.LIGHT_STATE.value)
+        msg.msg_types.append(MsgType.PACMAN_LOCATION.value)
+        msg.dir = direction
+        s.send(pack_msg(msg.SerializeToString(), _SUBSCRIBE))
+
+
+def msg_received(data, msg_type):
+        if msg_type != _SUBSCRIBE:
+            msg = message_buffers[MsgType(msg_type)]()
+            msg.ParseFromString(data)
+            # msg access syntax is in samplePacbotModule
+            print(msg)
+
+
+HOST = os.environ.get("LOCAL_ADDRESS", "localhost")
+PORT = os.environ.get("BIND_PORT", 11297)
+
+addr = (HOST, PORT)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(addr)
+print("connected")
+subscribe(MsgType, Subscribe.SUBSCRIBE)
+while True:
+    data = s.recv(2048)
+    magic, msg_type, length = SIZE_HEADER.unpack(
+                        data[:SIZE_HEADER.size])
+    buf = data[SIZE_HEADER.size:]
+    msg_received(buf[:length], msg_type)
